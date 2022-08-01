b454aebd190c1075b5c19a1c8594edd10d6a7151
dvhuydev <huydang.dev@gmail.com>
2022-08-01 09:19:49 +0700

    commit again

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+
+function checkPrime(n) {
+    let primeChecking = Math.sqrt(n);
+    const i = 2
+    for ( i ; i <= primeChecking; i++ ) {
+        if (n % i === 0) {
+            return;
+        }
+
+    }
+    return 1;
+}
+
+//Hàm vòng lặp
+function checkPrimenumber() {
+    const primeNumber = Number(document.getElementById("primeNumber").value);
+    let primeArray = [];
+    // Số nguyên tố là số chia dư 1
+    // Nếu = 2 hoặc chia = 0 thì số đó không phải số nguyên tố
+    
+    if (primeNumber < 2) {
+        alert(`Number ${primeNumber} is not a Prime Number. Please input another Number`);
+    }
+
+    const i = 2
+    for ( i ; i <= primeNumber; i++) {
+        if (checkPrime(i) === 1) {
+            // Push vào mảng nếu chia ra dư 1
+            // Đúng điều kiện của số nguyên tố
+            primeArray.push(i);
+        }
+    }
+    document.getElementById("primeResults").innerHTML = "Prime Number:" + " " + primeArray.join(", ")
+
+}
+checkPrimenumber();
